f21bd54a06502f1fac7eca5589317bf49bad75b1
Maxwell Flitton <maxwellflitton@Maxwells-MacBook-Pro.local>
2022-03-09 18:07:59 +0000

    adding main display

--- camel/interface/entry_points/main_interface.py
+++ camel/interface/entry_points/main_interface.py
@@ -1,5 +1,7 @@
 from camel.interface.components.camel_logo import print_camel_image
+from camel.interface.components.oasis_logo import print_logo_image
 
 
 def main():
     print_camel_image()
+    print_logo_image()
