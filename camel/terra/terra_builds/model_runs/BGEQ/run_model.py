7886691907c3fd1c2398f58bf5c8ad8155e50e28
maxwellflitton <maxwellflitton@gmail.com>
2022-03-29 17:32:21 +0100

    adding to known hosts

--- camel/terra/terra_builds/model_runs/BGEQ/run_model.py
+++ camel/terra/terra_builds/model_runs/BGEQ/run_model.py
@@ -12,18 +12,21 @@ if __name__ == "__main__":
                              help="the name of the ssh key being added")
     args = args_parser.parse_args()
 
-    sleep_count = 1
+    sleep_count = 2
     keep_waiting = True
 
     while keep_waiting is True:
         time.sleep(sleep_count)
-        sleep_count = sleep_count * 2
+        # sleep_count = sleep_count * 2
 
         if os.path.exists("./output.txt") is True:
             keep_waiting = False
             break
 
-    git_clone_command = Popen('ssh-keyscan -H "github.com" >> ~/.ssh/known_hosts && git clone git@github.com:OasisLMF/BangladeshCyclone.git', shell=True)
+    add_key = Popen('ssh-keyscan -H "github.com" >> ~/.ssh/known_hosts ', shell=True)
+    add_key.wait()
+
+    git_clone_command = Popen('git clone git@github.com:OasisLMF/BangladeshCyclone.git', shell=True)
     git_clone_command.wait()
 
     profile = "default"
@@ -66,5 +69,5 @@ if __name__ == "__main__":
 
     # get the data from s3
 
-    run_model = Popen("cd BangladeshCyclone/tests/test-1 && oasislmf model run --config oasislmf_mdk.json", shell=True)
+    run_model = Popen("cd BangladeshCyclone/BGWTCSS1/tests/test-1 && oasislmf model run --config oasislmf_mdk.json", shell=True)
     run_model.wait()
