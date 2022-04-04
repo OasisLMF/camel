d2d12d641e388a8797431f21adef4a3be5559bca
Kamal Charles <kamal.charles@oasispalmtree.co.uk>
2022-04-04 14:28:10 +0100

    Destroy terra-build by parsing --config_name

--- camel/terra/destroy_terra.py
+++ camel/terra/destroy_terra.py
@@ -5,8 +5,32 @@ import argparse
 import os
 from pathlib import Path
 from subprocess import Popen
+from typing import Any
 
 from camel.terra.config_loader import ConfigEngine
+from camel.storage.components.profile_storage import LocalProfileVariablesStorage
+from camel.terra_configs.components.config_mapper import TerraConfigMapper
+
+
+def _extract_variable(key: str, lookup_dict: dict, label: str) -> Any:
+    current_value = lookup_dict.get(key)
+
+    if current_value is None:
+        raise ValueError(f"{key} not found in {label} config")
+
+    if isinstance(current_value, str) and current_value[:2] == "=>":
+        local_storage = LocalProfileVariablesStorage()
+        current_value = local_storage.get(current_value[2:])
+
+        if current_value is None:
+            raise ValueError(f"{current_value[2:]} not found in profile storage")
+    return current_value
+
+
+def translate_dictionary(config: dict, label: str) -> dict:
+    for key in config.keys():
+        config[key] = _extract_variable(key=key, lookup_dict=config, label=label)
+    return config
 
 
 def main() -> None:
@@ -18,9 +42,20 @@ def main() -> None:
     config_parser = argparse.ArgumentParser()
     config_parser.add_argument('--config_path', action='store', type=str, required=False, default="terra_config.yml",
                                help="the path to the config yml file that defines the terraform build that is going to be destroyed (default: terra_config.yml)")
+    config_parser.add_argument('--config_name', action='store', type=str, required=False, default="none",
+                               help="the name of the existing terraform config file")
+
+    
     args = config_parser.parse_args()
 
-    config_path: str = str(os.getcwd()) + f"/{args.config_path}"
+    if args.config_name != "none":
+        print(f"running existing config: {args.config_name}")
+        config_map = TerraConfigMapper.get_cached_profile()
+        config_path: str = config_map.terra_builds_path + f"/{args.config_name}.yml"
+    else:
+        config_path: str = str(os.getcwd()) + f"/{args.config_path}"
+    
+    
     file_path: str = str(Path(__file__).parent) + "/terra_builds"
 
     config = ConfigEngine(config_path=config_path)
@@ -29,7 +64,8 @@ def main() -> None:
     variables = config["variables"]
 
     for key in variables:
-        command_buffer.append(f'-var="{key}={variables[key]}" ')
+        current_value = _extract_variable(key=key, lookup_dict=variables, label="terraform variables")
+        command_buffer.append(f'-var="{key}={current_value}" ')
 
     command = "".join(command_buffer)
 
