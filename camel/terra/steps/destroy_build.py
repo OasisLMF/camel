3074e9cfc82d06cb0b1a0f9a5b9e5f5f88953c3b
maxwellflitton <maxwellflitton@gmail.com>
2022-04-06 15:13:54 +0100

    adding step manager to the running of steps

--- camel/terra/steps/destroy_build.py
+++ camel/terra/steps/destroy_build.py
@@ -8,12 +8,30 @@ from camel.terra.steps.base import Step
 
 
 class DestroyBuild(Step):
+    """
+    This class is responsible for destroying a terraform build in a step process.
 
+    Attributes:
+        config: (dict) the terraform configuration for parameters for destroying the build
+        file_path: (str) the path to the terraform build files
+    """
     def __init__(self, config: dict, file_path: str) -> None:
+        """
+        The constructor for the DestroyBuild class.
+
+        Args:
+            config: (dict) the terraform configuration for parameters for destroying the build
+            file_path: (str) the path to the terraform build files
+        """
         self.config: dict = config
         self.file_path: str = file_path
 
     def run(self) -> None:
+        """
+        Destroys the terraform build.
+
+        Returns: None
+        """
         command_buffer = [f'cd {self.file_path}/{self.config["location"]} ', '&& ', 'terraform destroy ']
         variables = self.config["variables"]
 
