d53fa33ea9a488fe8016f410911eefc2f2f78224
maxwell flitton <maxwellflitton@maxwells-MacBook-Pro.local>
2022-09-16 10:24:35 +0100

    env vars added to run server

--- camel/terra/steps/run_command_on_server.py
+++ camel/terra/steps/run_command_on_server.py
@@ -44,7 +44,8 @@ class RunCommandOnServerStep(Step):
         terminal_command = TerminalCommand(command=command,
                                            environment_variables=self.environment_variables,
                                            ip_address=self.server_ip)
-        terminal_command.wait()
+        # print(terminal_command._compile_command())
+        # terminal_command.wait()
 
         # run_script = Popen(f"ssh -A -o StrictHostKeyChecking=no ubuntu@{self.server_ip} '{command}'", shell=True)
         # run_script.wait()
