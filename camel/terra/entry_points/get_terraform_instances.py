487cb51f0fb996573e9447280dfa1f73e80f53b5
maxwellflitton <maxwellflitton@gmail.com>
2022-12-21 18:25:14 +0000

    updating local variable

--- camel/terra/entry_points/get_terraform_instances.py
+++ camel/terra/entry_points/get_terraform_instances.py
@@ -4,6 +4,7 @@ This file defines the entry point for getting terraform instances currently on A
 # import argparse
 
 from camel.terra.components.live_ec2 import LiveEc2InstanceList
+from camel.storage.components.profile_storage import LocalProfileVariablesStorage
 
 
 def main() -> None:
@@ -15,7 +16,7 @@ def main() -> None:
     # parser.add_argument("--aws_secret_key", action="store", type=str, required=False, help="the AWS secret key")
     #
     # args = parser.parse_args()
-
+    LocalProfileVariablesStorage()
     raw_data: dict = LiveEc2InstanceList.get_raw_data_from_aws(region="eu-west-2")
     # instances: List[LiveEc2] = LiveEc2InstanceList(raw_data).get_instances_by_tag(key="Terraform", value="True")
     for instance in LiveEc2InstanceList(raw_data).instances:
