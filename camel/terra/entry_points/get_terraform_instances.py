1a39726f43276f696f5da9c8ff6936cf5cf5ab78
maxwellflitton <maxwellflitton@gmail.com>
2022-12-21 18:27:44 +0000

    updating locations

--- camel/terra/entry_points/get_terraform_instances.py
+++ camel/terra/entry_points/get_terraform_instances.py
@@ -17,7 +17,7 @@ def main() -> None:
     #
     # args = parser.parse_args()
     LocalProfileVariablesStorage()
-    raw_data: dict = LiveEc2InstanceList.get_raw_data_from_aws(region="eu-west-2")
+    raw_data: dict = LiveEc2InstanceList.get_raw_data_from_aws(region="eu-west-1")
     # instances: List[LiveEc2] = LiveEc2InstanceList(raw_data).get_instances_by_tag(key="Terraform", value="True")
     for instance in LiveEc2InstanceList(raw_data).instances:
         print(instance)
