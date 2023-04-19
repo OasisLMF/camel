6245a09f2c07d729f27f7f68f63c6aeb3173a1ad
KamalCharles <Kamal.charles@oasispalmtree.co.uk>
2023-04-19 15:53:20 +0100

    temp fix for install_terraform.sh location

---
+++ camel/terra/entry_points/scripts/install_terraform.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# This bash script is for downloading terraform on an EC2 instance.
+
+# get the latest version of terraform
+TERRAFORM_VER=`curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest |  grep tag_name | cut -d: -f2 | tr -d \"\,\v | awk '{$1=$1};1'`
+
+# download the latest version of terraform
+wget https://releases.hashicorp.com/terraform/${TERRAFORM_VER}/terraform_${TERRAFORM_VER}_linux_amd64.zip
+
+# install unzip so we can access the zip that we have just downloaded 
+sudo apt-get -y install unzip
+
+# unzip the terraform that we have just downloaded
+unzip terraform_${TERRAFORM_VER}_linux_amd64.zip
+
+# move the unzipped terraform file to the bash bin 
+sudo mv terraform /usr/local/bin/
+
+# remove the zipped file that we downloaded as we do not need it anymore
+rm terraform_${TERRAFORM_VER}_linux_amd64.zip
